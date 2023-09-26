6a5522422675a33e64133af69f9ac9a527af6311
Abhishek Dhondalkar <129784079+Abhishek-Jain-1925@users.noreply.github.com>
2023-09-26 15:04:40 +0530

    Update explore_page.py

--- utils/explore_page.py
+++ utils/explore_page.py
@@ -3,6 +3,50 @@ from utils.libraries import *
 
 def show_explore_page(cars):
 
+    st.write(
+        """
+        # Welcome to My Cars24.com
+
+## Featured Cars
+
+### Toyota Camry
+![Toyota Camry]([https://example.com/toyota-camry.jpg](https://cdni.autocarindia.com/utils/imageresizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/CamryModelImage.jpg))
+- **Year:** 2020
+- **Price:** Rs.20,000
+- **KM Driven:** 30,000 km
+
+### Honda Civic
+![Honda Civic]([https://example.com/honda-civic.jpg](https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.carwale.com%2Fhonda-cars%2Fcivic%2F&psig=AOvVaw3iwlz6dP6pQB8zEAm-Yroa&ust=1695806819490000&source=images&cd=vfe&opi=89978449&ved=0CA4QjRxqFwoTCLjq9uv6x4EDFQAAAAAdAAAAABAD))
+- **Year:** 2019
+- **Price:** Rs.18,500
+- **KM Driven:** 35,000 km
+
+### Tata Altroz
+![Tata Altroz]([https://example.com/honda-civic.jpg](https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.carwale.com%2Fhonda-cars%2Fcivic%2F&psig=AOvVaw3iwlz6dP6pQB8zEAm-Yroa&ust=1695806819490000&source=images&cd=vfe&opi=89978449&ved=0CA4QjRxqFwoTCLjq9uv6x4EDFQAAAAAdAAAAABAD))]
+- **Year:** 2023
+- **Price:** Rs.9,00,000
+- **KM Driven:** 10,000 km
+
+## Search for Cars
+
+Use the form below to search for cars:
+
+
+
+### About Us
+
+We are a trusted car dealership dedicated to helping you find the perfect vehicle.
+
+Contact us at [jainabhishek1925@gmail.com](mailto:jainabhishek1925@gmail.com).
+
+---
+
+**Disclaimer:** This is a simplified example and does not replicate the functionality or complexity of a website like Cars24.com. To create a website like Cars24.com, you would need to use web development technologies such as HTML, CSS, JavaScript, and potentially backend languages and databases to manage car listings and user interactions. Markdown is not suitable for creating fully functional web applications.
+
+
+        """
+    )
+
     st.title("Explore Cars Dataset")
 
     st.write(

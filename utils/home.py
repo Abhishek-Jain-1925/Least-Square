2d540c9439f80a3fec39a07cf9dbfa51479f5aa5
Abhishek Dhondalkar <129784079+Abhishek-Jain-1925@users.noreply.github.com>
2023-11-22 16:56:40 +0530

    Added via File Upload

---
+++ utils/home.py
@@ -0,0 +1,47 @@
+from utils.libraries import *
+
+
+def home(cars):
+    st.title(" Welcome to My Cars24.com - Home")
+    st.write(
+        """
+        
+
+## Featured Cars
+
+### Toyota Camry
+![Toyota Camry]([https://example.com/toyota-camry.jpg](https://cdni.autocarindia.com/utils/imageresizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/CamryModelImage.jpg))
+- **Year:** 2020
+- **Price:** Rs.20,000
+- **KM Driven:** 30,000 km
+
+### Honda Civic
+![Honda Civic]([https://example.com/honda-civic.jpg](https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.carwale.com%2Fhonda-cars%2Fcivic%2F&psig=AOvVaw3iwlz6dP6pQB8zEAm-Yroa&ust=1695806819490000&source=images&cd=vfe&opi=89978449&ved=0CA4QjRxqFwoTCLjq9uv6x4EDFQAAAAAdAAAAABAD))
+- **Year:** 2019
+- **Price:** Rs.18,500
+- **KM Driven:** 35,000 km
+
+### Tata Altroz
+![Tata Altroz]([https://example.com/honda-civic.jpg](https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.carwale.com%2Fhonda-cars%2Fcivic%2F&psig=AOvVaw3iwlz6dP6pQB8zEAm-Yroa&ust=1695806819490000&source=images&cd=vfe&opi=89978449&ved=0CA4QjRxqFwoTCLjq9uv6x4EDFQAAAAAdAAAAABAD))]
+- **Year:** 2023
+- **Price:** Rs.9,00,000
+- **KM Driven:** 10,000 km
+
+## Search for Cars
+
+Use the form below to search for cars:
+
+
+
+### About Us
+
+We are a trusted car dealership dedicated to helping you find the perfect vehicle.
+
+Contact us at [jainabhishek1925@gmail.com](mailto:jainabhishek1925@gmail.com).
+
+
+
+        """
+    )
+
+    

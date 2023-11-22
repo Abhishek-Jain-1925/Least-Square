6c29854e77533e575e7093b8d771b5be2de6923b
Abhishek Dhondalkar <129784079+Abhishek-Jain-1925@users.noreply.github.com>
2023-11-22 17:51:35 +0530

    Update home.py

--- utils/home.py
+++ utils/home.py
@@ -10,19 +10,19 @@ def home(cars):
 ## Featured Cars
 
 ### Toyota Camry
-![Toyota Camry]([https://example.com/toyota-camry.jpg](https://cdni.autocarindia.com/utils/imageresizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/CamryModelImage.jpg))
+![Toyota Camry](./images/Toyota-Camry.webp)
 - **Year:** 2020
 - **Price:** Rs.20,000
 - **KM Driven:** 30,000 km
 
 ### Honda Civic
-![Honda Civic]([https://example.com/honda-civic.jpg](https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.carwale.com%2Fhonda-cars%2Fcivic%2F&psig=AOvVaw3iwlz6dP6pQB8zEAm-Yroa&ust=1695806819490000&source=images&cd=vfe&opi=89978449&ved=0CA4QjRxqFwoTCLjq9uv6x4EDFQAAAAAdAAAAABAD))
+![Honda Civic](./images/honda-civic.webp)
 - **Year:** 2019
 - **Price:** Rs.18,500
 - **KM Driven:** 35,000 km
 
 ### Tata Altroz
-![Tata Altroz]([https://example.com/honda-civic.jpg](https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.carwale.com%2Fhonda-cars%2Fcivic%2F&psig=AOvVaw3iwlz6dP6pQB8zEAm-Yroa&ust=1695806819490000&source=images&cd=vfe&opi=89978449&ved=0CA4QjRxqFwoTCLjq9uv6x4EDFQAAAAAdAAAAABAD))]
+![Tata Altroz](./images/tata-altroz.webp)
 - **Year:** 2023
 - **Price:** Rs.9,00,000
 - **KM Driven:** 10,000 km

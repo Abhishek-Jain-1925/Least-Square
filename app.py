3a6f74496a4ed3c0b6efce5f77d1559817f69eb4
Abhishek Dhondalkar <129784079+Abhishek-Jain-1925@users.noreply.github.com>
2023-11-22 17:24:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from utils.functions import feature_engineering
+from utils.home import home
 from utils.predict_page import show_predict_page
 from utils.explore_page import show_explore_page
 from utils.model_page import compare_model_page
